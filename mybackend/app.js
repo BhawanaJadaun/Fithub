86511fd371ae5f5438f43157309c3d65fbbee84f
Bhawana Jadaun <154340487+BhawanaJadaun@users.noreply.github.com>
2025-05-08 13:25:12 +0530

    Update app.js

--- mybackend/app.js
+++ mybackend/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from 'cors';
-// import authRoutes from "./routes/auth.route.js";
-import userRouter from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 import adminRoutes from "./routes/adminRoutes.js";
 import gymRoutes from "./routes/gymRoutes.js";
 import AppError from "./utils/AppError.js";
@@ -43,7 +42,7 @@ app.get("/", (req, res) => {
 });
 
  //User api routes
- app.use('/api/v1/users',userRouter);
+ app.use('/api/v1/users',userRoutes);
   //admin api routes
   app.use('/api/v1/admin',adminRoutes);
   //Gym api routes
